5b1b881dec9d0ac761e4e3ab614794afdd4083c1
jacob <1063010784@qq.com>
2025-02-17 17:50:34 +0800

    multi

--- server/run.py
+++ server/run.py
@@ -7,4 +7,4 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from server.server import app
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) 
+    app.run(host='0.0.0.0', port=5000, threaded=True) 
